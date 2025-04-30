f4be91d961a698f9161bd84a0ab550fb6abb1b1d
jieun13 <ynb9598@gmail.com>
2025-04-30 23:41:38 +0900

    initial commit

---
+++ src/test/java/com/oss/saber/SaberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.saber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
